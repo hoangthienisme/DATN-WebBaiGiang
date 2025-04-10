95dba0a899ee1b7e3b0165ecac8cc8bbc8a5bf32
Nguyễn Hoàng Thiện <nguyenhoangthien120304@gmail.com>
2025-04-10 22:37:27 +0700

    Done CreateCourses

--- WebBaiGiang/WebBaiGiang/obj/Debug/net8.0/WebBaiGiang.AssemblyInfo.cs
+++ WebBaiGiang/WebBaiGiang/obj/Debug/net8.0/WebBaiGiang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBaiGiang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f72bdd603e4153b3706a17ef1d6aa2eab394a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a798850b25a93d59181c909233732c59b20f86b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBaiGiang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBaiGiang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
